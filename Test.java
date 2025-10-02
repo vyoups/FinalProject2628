997248b2613f5a308d93d59d017261d2967ef7db
vyoups <marcianorifat5@gmail.com>
2025-10-02 15:17:44 +0700

    Menambahkan kode pada barus ke 4

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
+        System.out.println("User 1 menambahkan baris baru");
     }
 }
